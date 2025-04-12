ee500bebf98a9de2875d78fb47213c61854657b8
Maciej.Koryl <maciej.koryl@asseco.pl>
2025-04-12 15:52:53 +0200

    Pobieranie kontaktów

--- src/main/java/edu/wsiiz/repairshop/communication/application/ContactController.java
+++ src/main/java/edu/wsiiz/repairshop/communication/application/ContactController.java
@@ -2,13 +2,11 @@ package edu.wsiiz.repairshop.communication.application;
 
 import edu.wsiiz.repairshop.communication.domain.contact.Contact;
 import edu.wsiiz.repairshop.communication.domain.contact.ContactRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/communication/contacts")
@@ -23,6 +21,12 @@ public class ContactController {
     val saved = repository.save(contact);
 
     return ResponseEntity.created(null).body(saved);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Contact>> getMany() {
+
+    return ResponseEntity.ok(repository.findAll());
   }
 
 }
