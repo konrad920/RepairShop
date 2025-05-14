5f52146c5b4782b9207dc07391926e078ef3c7e8
Wojciech Ganczar <woiciech5@gmail.com>
2025-05-14 02:13:32 +0200

    Dodanie jednostek produktów.

--- src/main/java/edu/wsiiz/repairshop/storage/domain/storage/Product.java
+++ src/main/java/edu/wsiiz/repairshop/storage/domain/storage/Product.java
@@ -1,7 +1,18 @@
 package edu.wsiiz.repairshop.storage.domain.storage;
 
+import lombok.Getter;
+
+@Getter
 public enum Product {
-    FENDER,
-    BUMPER,
-    DOOR
+    FENDER("cnt"),
+    BUMPER("cnt"),
+    DOOR("cnt"),
+    GREASE("kg"),
+    COOLANT("L");
+
+    final String unit;
+
+    Product(String unit) {
+        this.unit = unit;
+    }
 }
