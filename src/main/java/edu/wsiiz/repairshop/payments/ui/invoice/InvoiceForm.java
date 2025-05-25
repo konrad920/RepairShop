63b2acabbc2c81137a3074815710dc794602e0aa
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2025-05-25 10:52:29 +0200

    Dodano formularz InvoiceForm do tworzenia i edycji faktur
    Nowa klasa `InvoiceForm` umożliwia wprowadzanie danych faktury: klient, data wystawienia,
    status oraz kwota. Formularz używa Vaadin Binder do wiązania danych z obiektem `InvoiceEntity`.
    Dodano przyciski "Save" i "Cancel" oraz obsługę zdarzeń przez `Consumer` i `Runnable`, co umożliwia łatwe osadzanie w dialogu i przekazywanie danych do widoku nadrzędnego.
    
    - bindInstanceFields dla automatycznego mapowania pól
    - walidacja przed zapisem
    - elastyczne przypięcie logiki onSave / onCancel

--- src/main/java/edu/wsiiz/repairshop/payments/ui/invoice/InvoiceForm.java
+++ src/main/java/edu/wsiiz/repairshop/payments/ui/invoice/InvoiceForm.java
@@ -1,4 +1,80 @@
 package edu.wsiiz.repairshop.payments.ui.invoice;
 
-public class InvoiceForm {
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.BigDecimalField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.data.binder.Binder;
+import edu.wsiiz.repairshop.payments.domain.invoice.Invoice;
+import edu.wsiiz.repairshop.payments.domain.invoice.InvoiceStatus;
+
+
+import java.util.function.Consumer;
+
+
+public class InvoiceForm extends Composite<FormLayout> {
+
+    private final TextField clientField = new TextField("Client");
+    private final DatePicker issueDate = new DatePicker("Issue Date");
+    private final ComboBox<InvoiceStatus> statusBox = new ComboBox<>("Status");
+    private final BigDecimalField amountField = new BigDecimalField("Amount");
+
+    private final Button saveButton = new Button("Save");
+    private final Button cancelButton = new Button("Cancel");
+
+    private final Binder<Invoice> binder = new Binder<>(Invoice.class);
+    private Invoice invoice;
+
+    private Consumer<Invoice> onSave;
+    private Runnable onCancel;
+
+    public InvoiceForm() {
+        statusBox.setItems(InvoiceStatus.values());
+
+        getContent().add(clientField, issueDate, statusBox, amountField, createButtons());
+
+        binder.bindInstanceFields(this);
+        binder.setBean(new Invoice());
+    }
+
+    private HorizontalLayout createButtons() {
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        saveButton.addClickListener(e -> validateAndSave());
+        cancelButton.addClickListener(e -> {
+            if (onCancel != null) onCancel.run();
+        });
+
+        return new HorizontalLayout(saveButton, cancelButton);
+    }
+
+    private void validateAndSave() {
+        if (binder.validate().isOk()) {
+            if (onSave != null) {
+                onSave.accept(binder.getBean());
+            }
+        } else {
+            Notification.show("Please fix errors before saving.", 3000, Notification.Position.MIDDLE);
+        }
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+        binder.readBean(invoice);
+    }
+
+    public void setOnSave(Consumer<Invoice> onSave) {
+        this.onSave = onSave;
+    }
+
+    public void setOnCancel(Runnable onCancel) {
+        this.onCancel = onCancel;
+    }
 }
